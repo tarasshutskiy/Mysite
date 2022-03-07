3e20e93c71a02626f26888bce29fca774433c33f
taras_shapik <shapiktaras@gmail.com>
2022-03-07 17:39:29 +0200

    add update

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = PROJECT_ROOT / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
