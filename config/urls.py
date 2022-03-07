100e51172453e303ab7cf5e32ba39d571e9ea232
taras_shapik <shapiktaras@gmail.com>
2022-03-07 17:54:18 +0200

    app

--- config/urls.py
+++ config/urls.py
@@ -26,10 +26,13 @@ urlpatterns = [
     path('it_bot/', include('blog.urls')),
     path('api/', include('api.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns = [
         path('__debug__/', include('debug_toolbar.urls')),
-    ]+urlpatterns
+    ]
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
